726e3fe9b88a46cd1e52b2628424c9bb8ea10a5d
Vipin Saini <59192965+thevipinsaini@users.noreply.github.com>
2022-04-30 21:13:14 +0530

    Updated Code

--- Structured Data Testing.js
+++ Structured Data Testing.js
@@ -1,1 +1,1 @@
-javascript:void(window.open(%27https://validator.schema.org/?url=%27+window.location.href,%27_blank%27));
+javascript:(function(){ window.open('https://validator.schema.org/#url='+encodeURIComponent(location.href))})();
